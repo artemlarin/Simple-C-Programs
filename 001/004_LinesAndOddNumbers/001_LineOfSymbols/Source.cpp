21fbd6a9c6a7768c44fbde89a0ad2ece6a81c6f9
Artem <32986408+artemlarin@users.noreply.github.com>
2023-04-15 21:16:47 +0600

    LinesAndOddNumbers

---
+++ 001/004_LinesAndOddNumbers/001_LineOfSymbols/Source.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(0, "Rus");
+
+	char symbol;
+	int characters, choise, counter = 0;
+	bool examination;
+
+	cout << "\t\t\tЛиния из символов." << endl << endl;
+
+	cout << "Введите символ: ";
+	cin >> symbol;
+	cout << "Введите количество символов: ";
+	cin >> characters;	
+
+	do
+	{
+		cout << "Какую линию будем рисовать:" << endl
+			<< "\t1 - Горизонтальную." << endl
+			<< "\t2 - Вертикальную." << endl << endl;
+		cout << "Ваш выбор: ";
+		cin >> choise;
+
+		if (choise == 1 || choise == 2)
+			examination = false;
+		else
+		{
+			cout << "\nОшибка ввода!" << endl;
+			cout << "Повторите ввод." << endl << endl;
+			examination = true;
+		}
+	} while (examination);
+
+	switch (choise)
+	{
+		case 1:
+		{
+			cout << endl;
+			while (counter < characters)
+			{
+				cout << symbol;
+				counter++;
+			}
+		}
+		break;
+
+		case 2:
+		{
+			cout << endl;
+			while (counter < characters)
+			{
+				cout << symbol << endl;
+				counter++;
+			}
+		}
+		break;
+	}
+
+	cout << "\n\nВсё, готово!" << endl;
+	cout << "Программа завершена." << endl << endl;
+}
---
+++ 001/004_LinesAndOddNumbers/002_SumOfOdd/Source.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(0, "Rus");
+
+	int begin, end, sum, initialValue;
+	bool odd;
+
+	sum = 0;
+
+	cout << "\tНаходим сумму всех целых нечетных чисел в диапазоне." << endl << endl;
+	cout << "Введите начальное и конечное значение: ";
+	cin >> begin >> end;
+
+	initialValue = begin;
+	while (end >= begin)
+	{
+		odd = begin % 2 == 1;
+
+		if (odd)
+		{
+			sum += begin;
+		}
+		begin++;
+	}
+
+	cout << "Сумма всех нечетных чисел в диапазоне от " 
+		<< initialValue << " до " << end << ": " << sum << endl;
+}

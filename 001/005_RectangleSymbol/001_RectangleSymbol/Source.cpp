ddf3f1078d7da8fc5b396107b90e9348d9349fdb
Artem <32986408+artemlarin@users.noreply.github.com>
2023-04-26 21:13:05 +0600

    RectangleSymbol

---
+++ 001/005_RectangleSymbol/001_RectangleSymbol/Source.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(0, "Rus");
+
+	int height, width;
+
+	cout << "\t\tПрямоугольник символом *" << endl << endl;
+
+	cout << "Введите высоту прямоугольника: ";
+	cin >> height;
+	cout << "Введите ширину прямоугольника: ";
+	cin >> width;
+
+	cout << endl;
+	for (int  i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	cout << "\nВсе, готово!" << endl;
+	cout << "Программа завершена." << endl << endl;
+}
